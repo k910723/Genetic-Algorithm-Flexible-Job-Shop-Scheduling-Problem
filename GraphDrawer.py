9e38c525931da5068fc2a0ea16d78e6d6d24795d
unknown <k910723911@gmail.com>
2023-06-22 21:10:36 +0800

    commit

--- GraphDrawer.py
+++ GraphDrawer.py
@@ -1,4 +1,4 @@
-# 甘特图绘图类
+# Darw a Gantt chart
 import os
 import random
 
@@ -7,43 +7,39 @@ class GraphDrawer:
 	def draw_schedule(number_machines, max_operations, jobs, filename=None):
 		import matplotlib.pyplot as plt
 		import matplotlib.patches as patches
-		plt.rcParams['font.sans-serif'] = ['Microsoft YaHei '] # 指定图像默认字体 解决图例中文不识别之问题
-		plt.rcParams['axes.unicode_minus'] = False # 解决保存图像是负号'-'显示为方块的问题
-		# 操作条状图间的垂直距离
+		#plt.rcParams['font.sans-serif'] = ['Microsoft YaHei ']
+		plt.rcParams['axes.unicode_minus'] = False # solving the problem of symbol '-' being the save button
 		operation_vertical_space = 1
-		# 操作条状图高度
 		operation_vertical_height = 2
-		# 以machine id为索引 已完成的操作dict
+		# dictionary with machine ID as key
 		operations_done = {}
 		for job in jobs:
 			for activity in job.activities_done:
-				# 添加所有操作
+				# add all operations
 				operation = activity.operation_done
-				# 初始化list（若首次添加到machine对象）
 				if operations_done.get(operation.id_machine) is None:
 					list_operations = []
-				# 非首次添加直接获取已有的list
 				else:
 					list_operations = operations_done.get(operation.id_machine)
 
-				# 以JobID和ActivityID为标识符新添一条记录
+				# add a record with its job ID and activity ID
 				list_operations.append((job.id_job, activity.id_activity, operation))
-				# 更新字典
+				# update dictionary
 				operations_done.update({operation.id_machine: list_operations})
 
-		# 生成随机颜色块用于显示不同物料对应工序的颜色条
+		# generate different color
 		colors = ['#%06X' % random.randint(0, 256 ** 3 - 1) for _ in range(len(jobs))]
-		# 画图
+		# draw
 		plt.clf()
 		plot = plt.subplot()
 		for id_machine, list_operations in operations_done.items():
 			for id_job, id_activity, operation in list_operations:
-				# X 坐标对应操作时间，Y 坐标对应操作顺序
+				# X - time，Y - machine
 				x, y = operation.time, 1 + id_machine * max_operations * (
 						operation_vertical_space + operation_vertical_height) + operation.place_of_arrival * (
 							   operation_vertical_space + operation_vertical_height)
-				# 根据 operation 时间长度绘制矩形
-				if operation.id_operation == -1: # 故障黑块绘制
+				# draw according to operation time
+				if operation.id_operation == -1: # black block if machine breaks down
 					plot.add_patch(
 					patches.Rectangle(
 						(x, y-1),
@@ -62,24 +58,24 @@ class GraphDrawer:
 						)
 					)
 
-		# 以机器数量调整Y轴高度
+		# adjust Y height
 		plt.yticks([1 + (i + 1) * max_operations * (operation_vertical_space + operation_vertical_height) + (
 				max_operations * (operation_vertical_height + operation_vertical_space) - operation_vertical_space) / 2 for i in
 					range(number_machines)], ["CNC" + str(i + 1) + "#" for i in range(number_machines)])
-		# 自动缩放
+
 		plot.autoscale()
 
-		# 绘制JOB ID对应的图例
+		# draw according to job ID
 		handles = []
 		handles.append(patches.Patch(color="#000000", label='Failure'))
 		for id_job, color in enumerate(colors):
 			handles.append(patches.Patch(color=color, label='Item' + str(id_job + 1)))
 		plt.legend(handles=handles)
 
-		print("图像已在新窗口打开，关闭图形窗口可继续")
+		print("image opened in window, close to continue")
 
-		# 显示绘图
+		# show the chart
 		plt.show()
 		if not (filename is None):
 			plt.savefig(os.path.join("output", filename), bbox_inches='tight')
-			print("图像已保存")
+			print("image saved")
